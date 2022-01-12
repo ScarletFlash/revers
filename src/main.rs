8be4f22d27d3301b4ff427e6ae8a576c99d6f855
ScarletFlash <scarletflash.dev@gmail.com>
2022-01-12 23:29:17 +0300

    feat: add clap for CLI arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{App, AppSettings, ArgMatches};
+
+mod subcommands;
+use subcommands::{changelog, commit_hashes, commit_messages, version};
+
 fn main() {
-    println!("Hello, world!");
+    let matches: ArgMatches = App::new("revers")
+        .bin_name("revers-cli")
+        .about("CLI for customizable version management")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::AllowExternalSubcommands)
+        .setting(AppSettings::AllowInvalidUtf8ForExternalSubcommands)
+        .setting(AppSettings::DisableHelpSubcommand)
+        .setting(AppSettings::DisableVersionFlag)
+        .subcommand_value_name("COMMAND")
+        .subcommand_help_heading("COMMANDS")
+        .subcommand(commit_hashes::get_app())
+        .subcommand(changelog::get_app())
+        .subcommand(commit_messages::get_app())
+        .subcommand(version::get_app())
+        .get_matches();
+
+    commit_hashes::handle_matches(&matches);
+    changelog::handle_matches(&matches);
+    commit_messages::handle_matches(&matches);
+    version::handle_matches(&matches);
 }
---
+++ src/subcommands/mod.rs
@@ -0,0 +1,4 @@
+pub mod changelog;
+pub mod commit_hashes;
+pub mod commit_messages;
+pub mod version;
